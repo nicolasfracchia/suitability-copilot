22c2458614e14aac2620550a3d703412aae72cbc
Nicolas Fracchia <dev.nfracchia@gmail.com>
2026-03-01 23:37:31 -0700

    chore: added openai dependency and OPENAI_API_KEY config

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL")
+    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
+
 
 settings = Settings()
